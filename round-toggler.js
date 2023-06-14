5b221bf99acd5fb86523c7871ab075b961334f89
zeroIsScared <69678222+zeroIsScared@users.noreply.github.com>
2023-06-14 22:15:09 +0300

    Update round-toggler.js
    adding a helper function

--- round-toggler.js
+++ round-toggler.js
@@ -9,10 +9,10 @@ let btn2 = document.getElementsByTagName('button')[0];
 let btnByClass = document.getElementsByClassName('plus')[0];
 let btnById = document.getElementById('plusButton');
 
-console.log(btn1);
-console.log(btn2);
-console.log(btnByClass);
-console.log(btnById);
+// console.log(btn1);
+// console.log(btn2);
+// console.log(btnByClass);
+// console.log(btnById);
 
 
 //Add an event listener 
@@ -20,9 +20,12 @@ console.log(btnById);
 let n = 0;
 console.log(n);
 
-//add a helper function
-const bntSize= function (size, k) {
-    return btn.style.size= `${parseInt(btn.style.size) * k}px`;
+//add a helpper function
+const btnSize= function (currentHeight, k) {
+    let newHeight= currentHeight * k;
+    btn.style.height= `${newHeight}px`;
+    btn.style.width= `${newHeight}px`;
+    btn.style.borderRadius= `${newHeight}px`;
 }
 
 
@@ -30,21 +33,17 @@ btn.addEventListener('click', () => {
     //read the button width and height
 
     let comStyle = getComputedStyle(btn);
-    let initialHeight = parseInt(comStyle.height);
 
     if (n === 2) {
-        btnSize('height', 100/225);
-        btnSize('width', 100/225);
-        //adjust the border radius
-        btnSize('borderRadius', 100/225);        
+
+        btnSize(parseInt(btn.style.height), 100/225);              
         n = 0;
+
     } else {
 
-        btnSize('height', 1.5);
-        btnSize('width', 1.5);
-        //adjust the border radius
-        btnSize('borderRadius', 1.5);   
+        btnSize(parseInt(comStyle.height), 1.5);
         n++;
+
     }
 
 })
